119380e5984ebb00cbf69924df2632a494f97ac1
DemidovVladimir <uncojet@gmail.com>
2019-05-04 18:33:02 +0200

    feat(stitch): Add Mongo Stitch and Atlas

--- reducers/db-reducers.js
+++ reducers/db-reducers.js
@@ -1,23 +1,32 @@
 import {actionTypes} from '../actions/db-actions';
 
-export const exampleInitialState = {
-    client: null,
-    db: null,
-    error: null,
-    users: null
+export const initialState = {
+    error: undefined,
+    users: undefined,
+    stitchClient: undefined,
+    currentUser: undefined
 }
 
 // REDUCERS
-export const dbReducer = (state = exampleInitialState, action) => {
+export const dbReducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.GET_DB_CLIENT_SUCCESS:
-            return {...state, client: action.payload}
-        case actionTypes.GET_DB_SUCCESS:
-            return {...state, db: action.payload}
+        case actionTypes.GET_STITCH_CLIENT_SUCCESS:
+            return {...state, stitchClient: action.payload}
+        case actionTypes.USER_LOG_IN_SUCCESS:
+            return {...state, currentUser: action.payload}
+        case actionTypes.USER_LOG_IN_FAIL:
+            return {...state, error: action.error}
+        case actionTypes.USER_LOG_OUT:
+            return {...state, currentUser: undefined}
         case actionTypes.FAIL_CONNECTION:
             return {...state, error: action.payload}
         case actionTypes.GET_USERS_SUCCESS:
-            return {...state, users: action.payload}
+            const users = action.payload.map(user => ({
+                key: user.name
+            }));
+            return {...state, users}
+        case actionTypes.GET_USERS_FAIL:
+            return {...state, error: action.payload}
         default:
             return state
     }
